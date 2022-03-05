1154f3a85de09bcea6137b8671df1854c9512522
Benja <abarzuabenjamin@gmail.com>
2022-03-05 10:54:40 -0300

    added mutable grid

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const container = document.getElementById("grid");
-for (let i = 0; i < 256; i++) {
-    const cell = document.createElement("div");
-    cell.className = 'cell'
-    container.appendChild(cell); 
+function createGrid(side) {
+    const container = document.getElementById("box");
+    const containerSize = parseInt(getComputedStyle(container).width)
+    const cellSize = (containerSize / side)
+    const cellTotal = side * side
+    clearGrid()
+    container.appendChild(createCell(cellTotal, cellSize))
 }
 
+function clearGrid() {
+    document.querySelectorAll('.cell').forEach(element => {
+        element.remove()
+    });
+}
+
+function createCell(cellTotal, cellSize) {
+    const grid = document.createElement('div')
+    grid.className = 'grid'
+    for (let i = 0; i < cellTotal; i++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell'
+        cell.style.width = cellSize + 'px'
+        cell.style.height = cellSize + 'px'
+        grid.appendChild(cell);
+    }
+    return grid
+}
