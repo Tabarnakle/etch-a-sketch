7772d66680f1d6e0902972a4c7b624d1eeb06738
Benja <abarzuabenjamin@gmail.com>
2022-03-05 12:41:15 -0300

    added buttons

--- script.js
+++ script.js
@@ -3,25 +3,38 @@ function createGrid(side) {
     const containerSize = parseInt(getComputedStyle(container).width)
     const cellSize = (containerSize / side)
     const cellTotal = side * side
-    clearGrid()
-    container.appendChild(createCell(cellTotal, cellSize))
+    // clearGrid()
+    container.appendChild(createCells(cellTotal, cellSize))
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.addEventListener('mouseover', event => {
+            cell.style['background-color'] = 'black'
+        })
+    })
 }
 
 function clearGrid() {
-    document.querySelectorAll('.cell').forEach(element => {
-        element.remove()
-    });
+    let cells = document.querySelectorAll('.cell')
+    if (cells) {
+        cells.forEach(element => {
+            element.remove()
+    })
+    } else {
+        console.log('No cells created');
+    }
 }
 
-function createCell(cellTotal, cellSize) {
+function createCells(cellTotal, cellSize) {
     const grid = document.createElement('div')
     grid.className = 'grid'
     for (let i = 0; i < cellTotal; i++) {
         const cell = document.createElement('div');
         cell.className = 'cell'
+        // cell.id = `cell-${i}`
         cell.style.width = cellSize + 'px'
         cell.style.height = cellSize + 'px'
         grid.appendChild(cell);
     }
     return grid
 }
+document.getElementById('btnClear').addEventListener('click', () => clearGrid())
+document.getElementById('btnCreate').addEventListener('click', () => createGrid(16))
